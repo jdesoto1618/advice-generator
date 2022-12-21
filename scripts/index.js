50eb29b7f3964282bdb94d136d6704642cd34618
Joe DeSoto <modulusman@gmail.com>
2022-12-21 14:44:34 -0800

    Adds advice API functionality on load and also on click of the dice button.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,21 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("hello from index js");
+  const adviceGenerator = document.querySelector(".advice-generator-button");
+  const BASE_URL = "https://api.adviceslip.com/advice";
+  const adviceId = document.querySelector(".advice-number");
+  const adviceTextElement = document.querySelector(".advice-text");
+
+  const getAdvice = async (url) => {
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        adviceId.innerHTML = data.slip.id;
+        adviceTextElement.innerHTML = data.slip.advice;
+      });
+  };
+
+  getAdvice(BASE_URL);
+
+  adviceGenerator.addEventListener("click", () => {
+    getAdvice(BASE_URL);
+  });
 });
